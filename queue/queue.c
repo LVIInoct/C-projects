c98c554726d8c7c6beb57abf28f8a487a2228da3
@noctiluzenit <bonbietotallypro@gmail.com>
2026-09-02 20:47:14 -0300

    Removed unnecessary comments & added coherency throughout the code

--- queue/queue.c
+++ queue/queue.c
@@ -3,13 +3,13 @@ First Out order, dynamic array rules, and with a time complexity of O(1) for enq
 traversing and O(1) for checking size. */
 
 #include <stdio.h>
-#include <stdbool.h> // for boolean data type
-#include <stdlib.h> // for malloc
-
+#include <stdbool.h>
+#include <stdlib.h> 
+// declaring a node constructor
 struct Node {
-    int data;           // data
-    struct Node* next;  // Node* is a POINTER, trav is the pointer
-}; // node struct
+    int data;
+    struct Node* next;
+};
 
 // prototypes
 void peekFirst(struct Node* head); 
@@ -25,15 +25,14 @@ int main(void){
     struct Node* tail = NULL; // defining tail
     int choice;
     while (1){
-        // menu
-            printf("What would you like to do?\n 1) enqueue \n 2) poll \n 3) Peek first (head)\n 4) peek last (tail) \n 5) Check size of array and content\n 6)Exit\n");
+            printf("What would you like to do?\n 1) enqueue \n 2) poll \n 3) Peek first (head)\n 4) peek last (tail) \n 5) Check size of array and content\n 6) Exit\n");
             scanf("%d", &choice);
             switch (choice) {
                 case 1: { // enqueue first element
                     int value; // element value
-                    printf("Enter value:\n > "); // prompt for element value
-                    scanf("%d", &value); // takes element value as input
-                    enqueue(&head, &tail, value); // sends it to the Queue function to be added to the list
+                    printf("Enter value:\n> "); 
+                    scanf("%d", &value);
+                    enqueue(&head, &tail, value);
                     break;
                     }
                 case 2:
@@ -52,69 +51,65 @@ int main(void){
                     clear(&head, &tail); // free any leftovers in the list before exiting
                     printf("Exiting program & clearing leftovers.\n");
                     return 0; // force exit
-                default: // the 'else' of switch
-                    printf("Invalid choice.\n");
-                    continue; // if the user enters an invalid choice, prompt them again without exiting the program
+                default:
+                    printf("Invalid choice. (Out of bounds)\n");
+                    continue;
             }
     }
 
 }
 
-// Functions
 
 // enqueueing
 void enqueue(struct Node** head, struct Node** tail, int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node)); // create new node and pointer to new node
-    /* - malloc is dynamic memory management, so it stores memory and the address of the byte of this node in RAM.
-    it is necessary given that every new node needs its own space */
-    if (newNode == NULL) { // if next node is null after trying to create it, then memory allocation failed
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
     printf("Memory allocation failed. (Somehow)\n");
-    return; // return user
+    return;
     }
-    newNode->data = data; // set data for this new node
-    newNode->next = NULL; // This node is at the very end (tail), so no one is behind it, so next is NULL.
+    newNode->data = data;
+    newNode->next = NULL;
     if (*head == NULL) { 
-        // If the list is empty, this person is the first AND the last.
         *head = *tail = newNode;
     }
     else {
-        (*tail)->next = newNode; // The current last node's next pointer now points to this new node.
-        *tail = newNode; // Update the tail pointer to point to this new node, which is now the last node in the list and official tail.
+        (*tail)->next = newNode;
+        *tail = newNode; 
     }
 }
 
 // peek first
-void peekFirst(struct Node* head) { // peeks head
-    if (head == NULL) { // if there is no head, then the list is empty
-        printf("List is empty\n");
+void peekFirst(struct Node* head) { 
+    if (head == NULL) {
+        printf("Queue is empty.\n");
         return;
     }
     else {
-        printf("First element: %d\n", head->data); // show data of first element ever added
+        printf("First element: %d\n", head->data);
     }
     printf("\n");
 
 }
 // peek last
 void peekLast(struct Node* tail){
-    if (tail == NULL) { // if there is no tail , then the list is empty
-        printf("List is empty\n");
+    if (tail == NULL) {
+        printf("Queue is empty.\n");
         return;
     }
     else {
-        printf("Last element: %d\n", tail->data); // show data of last element ever added
+        printf("Last element: %d\n", tail->data);
     }
 }
 // poll
 void poll(struct Node** head, struct Node** tail){
     if (*head == NULL){
-        printf("Queue is empty.");
+        printf("Queue is empty.\n");
         *tail = NULL;
         return;
-    } // if there's no head after polling, then the list is empty, so tail should also be NULL
-    struct Node* temp = *head; // temporary pointer to the current head to remove it
-    printf("Removed: %d\n", temp->data); // print what's being removed before freeing it
-    *head = (*head)->next; // point head to next that comes afterwards
+    }
+    struct Node* temp = *head;
+    printf("Removed: %d\n", temp->data);
+    *head = (*head)->next;
     if (*head == NULL){
         *tail = NULL;
     }
@@ -123,31 +118,29 @@ void poll(struct Node** head, struct Node** tail){
 
 // display list
 void display(struct Node* head) {
-    struct Node* trav = head; // start at the beginning
-    int size = 0; // initialize size counter
-    if (trav == NULL) { // if there is no head, then the list is empty
-        printf("List is empty\n");
+    struct Node* trav = head;
+    int size = 0;
+    if (trav == NULL) {
+        printf("Queue is empty.\n");
         return;
     }
-    while (trav != NULL) { // traverse through the list until we reach the end (where trav is NULL)
-        printf("[%d] ", trav->data); // print the data of each node
-        size++; // increment size counter for each node
-        trav = trav->next; // move to the next node
+    while (trav != NULL) {
+        printf("[%d] ", trav->data);
+        size++;
+        trav = trav->next;
     }
     printf("\n");
     printf("Size: %d\n", size);
 }
-// get rid of any leftovers after leaving
+// clear data
 void clear(struct Node** head, struct Node** tail) {
-    struct Node* trav = *head; // starting node
-    struct Node* nextNode;    // pointer
+    struct Node* trav = *head;
+    struct Node* nextNode;
     while (trav != NULL) {
         nextNode = trav->next;
-        free(trav);           // trav goes through the whole list, freeing each node as it goes, until it reaches the end (NULL)
+        free(trav);
         trav = nextNode;
-        /* important note: only free stuff only before setting their value to NULL, otherwise we will lose the
-        address of the memory we want to free and cause a memory leak*/
     }
-    *head = NULL; // set head and tail to null after freeing everything, so it doesn't point to any of the freed memory
+    *head = NULL;
     *tail = NULL;
 }
